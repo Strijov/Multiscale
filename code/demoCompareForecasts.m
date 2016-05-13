49e3bafd8bfdb9b55ff944383648d3fae14f55ed
Strijov <strijov@gmail.com>
2016-05-14 01:01:45 +0200

    Addend time series data loader template.

--- code/demoCompareForecasts.m
+++ code/demoCompareForecasts.m
@@ -1,25 +1,28 @@
 %Data preparation. Time series length are fixed and precomputed.
-addpath(genpath(cd));
-filename = 'data/orig/SL2.xls';
-sheet = 'Arkusz1';
-xlRange = 'D3:AA1098';
-ts0 = xlsread(filename,sheet,xlRange);
-ts0 = reshape(ts0', numel(ts0), 1);
+% addpath(genpath(cd));
+% filename = 'data/orig/SL2.xls';
+% sheet = 'Arkusz1';
+% xlRange = 'D3:AA1098';
+% ts0 = xlsread(filename,sheet,xlRange);
+% ts0 = reshape(ts0', numel(ts0), 1);
+% 
+% tmp = xlsread('data/orig/weatherdata.xls', 'weatherdata', 'E2:J1093');
+% ts{1} = ts0;
+% for i = [1:size(tmp, 2)]
+%     ts{i+1} = tmp(:, i);
+% end
+% ts_length = 155;
+% ts_legend = {'Consumption', 'Max Temperature','Min Temperature','Precipitation','Wind','Relative Humidity','Solar'};
+% time_step = {1, 24,24,24,24,24,24};
+% self_deltaTp = {6*24,6,6,6,6,6,6};
+% self_deltaTr = {24,1,1,1,1,1,1};
+% tmp1 = linspace(numel(ts{2}), numel(ts{2}) - 7 * ts_length, ts_length+1);
+% tmp2 = linspace(numel(ts{1}), numel(ts{1}) - 24 * 7 * ts_length, ts_length+1);
+% time_points = {tmp2,tmp1,tmp1,tmp1,tmp1,tmp1, tmp1};
+% inputStructTS = struct('x', ts, 'time_step', time_step, 'legend', ts_legend, 'deltaTp', self_deltaTp, 'deltaTr', self_deltaTr, 'time_points', time_points);
 
-tmp = xlsread('data/orig/weatherdata.xls', 'weatherdata', 'E2:J1093');
-ts{1} = ts0;
-for i = [1:size(tmp, 2)]
-    ts{i+1} = tmp(:, i);
-end
-ts_length = 155;
-ts_legend = {'Consumption', 'Max Temperature','Min Temperature','Precipitation','Wind','Relative Humidity','Solar'};
-time_step = {1, 24,24,24,24,24,24};
-self_deltaTp = {6*24,6,6,6,6,6,6};
-self_deltaTr = {24,1,1,1,1,1,1};
-tmp1 = linspace(numel(ts{2}), numel(ts{2}) - 7 * ts_length, ts_length+1);
-tmp2 = linspace(numel(ts{1}), numel(ts{1}) - 24 * 7 * ts_length, ts_length+1);
-time_points = {tmp2,tmp1,tmp1,tmp1,tmp1,tmp1, tmp1};
-inputStructTS = struct('x', ts, 'time_step', time_step0);%, 'legend', ts_legend, 'deltaTp', self_deltaTp, 'deltaTr', self_deltaTr, 'time_points', time_points);
+inputStructTS = LoadTimeSeriesSheaf('SL2');
+%Constructing regression matrix.
 [workStructTS] = CreateRegMatrix(inputStructTS);
 
 names_vector = cellstr(char('VAR', 'Neural_network', 'SVR'));
@@ -58,3 +61,7 @@ for i = [1:3]
 end
 table(MAPE_target, MAPE_full, AIC, 'RowNames', names_vector)
 
+
+/Applications/MATLAB_R2016a.app/toolbox/nnet/nnet/@network/network.m
+
+/Applications/MATLAB_R2016a.app/toolbox/nnet/nnutils/+nntype/network.m
