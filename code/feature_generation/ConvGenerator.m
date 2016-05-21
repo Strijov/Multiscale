5004990be0b0e7ac5ca61deafc637b0f0516ee2f
Anastasia874 <anastasia.motrenko@gmail.com>
2016-05-21 10:36:56 +0300

    removed unnecessary loop, statistics are computed only for X part of the target matrix

--- code/feature_generation/ConvGenerator.m
+++ code/feature_generation/ConvGenerator.m
@@ -1,9 +1,21 @@
 function add_features = ConvGenerator(workStructTS)
-    n = 5;
-    add_features = zeros(size(workStructTS.matrix,1), n);
+%Generates new features based on statistics of the current feature matrix.
+%
+% Input:
+% workStructTS	see createRegMatrix.m for explanation
+% 	workStructTS.matrix = [X Y]  contains the feature matrix X[m x deltaTp] 
+%    horiontally concatenated with the target matrix Y[m x deltaTr]
+%
+% Output:
+% workStructTS with new feature matrix
+    X = workStructTS.matrix(:, 1:workStructTS.deltaTp);
+    add_features = [sum(X, 2), mean(X, 2), min(X, [], 2), max(X, [], 2), std(X, 0, 2)];    
+    
+    %{
     for i = [1:size(workStructTS.matrix,1)]
-        x = workStructTS.matrix(i,:);
+        x = workStructTS.matrix(i,:); % AM ? Only use first deltaTp of the matrix
         y = [sum(x), mean(x), min(x), max(x), std(x)]; % real(fft(x)), imag(fft(x))];
         add_features(i, :) = y;
     end
+    %}
 end
