d0e671f9214269fc8cb1d2126b7f5eb9e5106ccb
Anastasia874 <anastasia.motrenko@gmail.com>
2016-05-27 16:16:50 +0300

    loaders for raw data updated

--- code/load_data/LoadEnergyWeatherTS.m
+++ code/load_data/LoadEnergyWeatherTS.m
@@ -4,15 +4,6 @@ function tsSheaf = LoadEnergyWeatherTS(dirname)
 % The structure ts has the fields: % FIXIT please move this help where this
 % structure is defined and put the reference in.
 %
-% Describes time series 
-% t [T,1] Time in milliseconds since 1/1/1970 (UNIX format)
-% x [T, N] Columns of the matrix are time series; missing values are NaNs
-% legend {1, N} Time series descriptions ts.x, e.g. ts.legend={?Consumption, ?Price?, ?Temperature?};
-% readme [string] Data information (source, formation time etc.)
-% type [1,N] (optional) Time series types ts.x, 1-real-valued, 2-binary, k ? k-valued
-% timegen [T,1]=func_timegen(timetick) (optional) Time ticks generator, may
-% contain the start (or end) time in UNIX format and a function to generate the vector t of the size [T,1]
-%
 % Iput:
 % dirname [string] named of folder with the loaded data. 
 % Output:
@@ -23,58 +14,59 @@ folder_name = folders{end-1};
 
 readme = struct('orig', 'Original time energy-weather series',...
                 'missing_value', 'Energy-weather time series with artificially inserted missing values',...
-                'varying_rate', 'Energy-weather time series with varying sampling rate');
+                'varying_rates', 'Energy-weather time series with varying sampling rate');
 readme = readme.(folder_name);
 
 [filename_train, filename_test, filename_weather] = read_missing_value_dir(dirname);    
 
-
 tsSheaf = cell(1, 2*numel(filename_train));
 for i = 1:numel(filename_train)
-    [train_ts, test_ts, train_weather, test_weather] = load_train_test_weather(filename_train{i},...
+    [train_ts, test_ts, time_train, time_test, train_weather, test_weather] = load_train_test_weather(filename_train{i},...
                                              filename_test{i},...
                                              filename_weather{i});
                                          
                                     
-    tsSheaf{2*i-1} = make_ts_struct(train_ts, train_weather, filename_train{i}, folder_name, readme);
-    tsSheaf{2*i} = make_ts_struct(test_ts, test_weather, filename_test{i}, folder_name, readme);
+    tsSheaf{2*i-1} = make_struct(train_ts, train_weather, time_train, filename_train{i}, folder_name, readme);
+    tsSheaf{2*i} = make_struct(test_ts, test_weather, time_test, filename_test{i}, folder_name, readme);
     
 end
 
 
 end
 
-function tsSheaf = make_ts_struct(target_ts, weather_data, filename, folder_name, readme)
+function ts_struct = make_struct(target_ts, weather_data, time, filename, folder_name, readme)
 
 
 ts = [target_ts, num2cell(weather_data, 1)];
-    time_step = [ {1} , num2cell(ones(1, size(weather_data, 2))*24) ];
     self_deltaTp = num2cell([24, ones(1, size(weather_data, 2))]*6); % ???
     self_deltaTr = num2cell([24, ones(1, size(weather_data, 2))]);
-
-    ts_length = 155; %size(target_ts, 1);
-    tmp1 = linspace(size(weather_data, 1), numel(ts{2}) - 7 * ts_length, ts_length+1);
-    tmp2 = linspace(numel(target_ts), numel(target_ts) - 24 * 7 * ts_length, ts_length+1);
-
 
     [~, fname, ~] = fileparts(filename);
     fname = strcat(folder_name, '_', fname);
     fname = regexprep(fname, '\.', '_');
     
-    time_points = {tmp2,tmp1,tmp1,tmp1,tmp1,tmp1, tmp1};
-    tsSheaf = struct('x', ts, 'time_step', time_step, ...
+    weather_time = time(1:24:numel(target_ts));
+    time = {time, weather_time, weather_time, weather_time, weather_time, weather_time, weather_time};
+    
+    ts_struct = struct('x', ts, 'time', time, ...
+                        'nsamples', self_deltaTr,...
+                        'legend', {'target', 'Max Temperature', 'Min Temperature', 'Precipitation', 'Wind', 'Relative Humidity', 'Solar'},...
                         'deltaTp', self_deltaTp, 'deltaTr', self_deltaTr,...
-                        'time_points', time_points, 'normalization', [], ...
-                        'name', fname, 'readme', readme);
-
+                        'name', fname, 'readme', readme,...
+                        'dataset', 'EnergyWeather');
+    %save(fullfile('data','ProcessedData', [ts_struct(1).name, '.mat']), 'ts_struct')
 end
 
 
-function [train_ts, test_ts, train_weather, test_weather] = load_train_test_weather(train, test, weather)
+
+function [train_ts, test_ts, time_train, time_test, train_weather, test_weather] = load_train_test_weather(train, test, weather)
 [~, ~, extension] = fileparts(train);
+
+time_train = []; 
+time_test = [];
 if strcmp(extension, '.csv')
-    test_ts = ProcessCSVOutput(test);
-    train_ts = ProcessCSVOutput(train);
+    [test_ts, time_test] = ProcessCSVOutput(test);
+    [train_ts, time_train] = ProcessCSVOutput(train);
     weather_data = csvread(weather, 1, 1);
     if size(weather_data, 2) > 6
         weather_data(:, 7:end) = [];
@@ -100,7 +92,8 @@ end
 function [ts, time_stamps, other] = ProcessCSVOutput(filename)
 
 ts = csvread(filename, 1, 0);
-time_stamps = ts(:, 1);
+% add hourly time stamps:
+time_stamps = add_hours_to_dates(ts(:, 1), 1:24);
 other = ts(:, 2:3);
 ts = ts(:, 4:end);
 ts = reshape(ts, numel(ts), 1);
@@ -113,32 +106,27 @@ end
 
 end
 
-function [ts, time_stamps, other] = ProcessXLSOutput(filename)
+function date_vec = add_hours_to_dates(yyyymmdd_vec, hours)
 
-[num, txt] = xlsread(filename,'', '','basic');
+n_dates = numel(yyyymmdd_vec);
 
-txt = txt(end-1095:end, :);
-idx_floats = ~cellfun('isempty', txt);
-ts = num;
+% duplicate time stamps to insert hourly recods:
+yyyymmdd_vec = repmat(yyyymmdd_vec', numel(hours), 1);
+yyyymmdd_vec = yyyymmdd_vec(:);
 
-try
-ts(idx_floats) = cellfun(@str2num, txt(idx_floats));
-catch
-   new_cell_mat = cellfun(@str2num, txt(end-1095:end, :), 'un', 0);
-   idx_fail = cellfun('isempty', new_cell_mat);
-   new_cell_mat(idx_fail) = {NaN};
-   ts = cell2mat(new_cell_mat);
-end
-time_stamps = ts(:, 1);
-other = ts(:, 2:3);
-ts = ts(:, 4:end);
-ts = reshape(ts, numel(ts), 1);
+% convert to 'yyyymmdd' to serial date numbers:
+date_serial = datenum(floor(yyyymmdd_vec/10000), ...
+                      floor(mod(yyyymmdd_vec,10000)/100),...
+                      floor(mod(yyyymmdd_vec,100))); 
+                  
+% Convert serials to numeric rows [year, month, day, hour, min, sec];                  
+date_vec = datevec(date_serial); 
 
-if numel(ts) ~= 24*1096
-    disp([filename, ': Data size: ', num2str(numel(ts)),...
-        ' does not match the expected size 26304 = 24x1096']);
-    
-end
+% add hours
+date_vec(:, 4) = date_vec(:, 4) + repmat(hours(:), n_dates,1);
+
+% back to serials:
+date_vec = datenum(date_vec);
 
 end
 
--- code/load_data/LoadNNcompetition.m
+++ code/load_data/LoadNNcompetition.m
@@ -1,4 +1,4 @@
-function ts_struct = LoadNNcompetition(dirname)
+function ts_struct = LoadNNcompetition(~)
 
 fname = 'NN3_final_data.csv';
 meta_fname = 'NN3_final_meta.csv';
@@ -6,24 +6,44 @@ meta_fname = 'NN3_final_meta.csv';
 readme = 'Empirical business monthly time series. Data from the 2006/07 Forecasting Competition for Neural Networks';
 
 % read data as a Table: 
-data_table = readtable(fname);   
+data_table = readtable(fname); 
+meta = readtable(meta_fname);
 headers = data_table.Properties.VariableNames;
 ts_struct = cell(1, numel(headers));
+
 for i =1:numel(headers)
     name = headers{i};
+    year = str2double(meta.(name){2});
     ts = data_table.(name);
-    ts = ts(~isnan(ts)); % some time series are shorter than others;
-    % arbitrarily set deltaTp = 12 (a year) and deltaTr = 6
+    
+    % some time series are shorter than others:
+    ts = ts(~isnan(ts)); 
+    
+    % arbitrarily set deltaTp = 12 (a year)
     deltaTp = 12;
     deltaTr = 6;
-    time_points = numel(ts):-1:deltaTr+deltaTp;
-    ts_struct{i} = struct('x', ts, 'time_step', 1, ...
+    time = monthly_time_from_year(year, numel(ts));
+    ts_struct = struct('x', ts, 'time', time, ...
                         'deltaTp', deltaTp, 'deltaTr', deltaTr,...
-                        'time_points', time_points, 'normalization', [], ...
                         'name', name, 'readme', readme, ...
-                        'plot_handle', @plot_ts, 'dataset', 'NNcompetition');
+                        'dataset', 'NNcompetition');
+                    
+     %save(fullfile('data','ProcessedData', [ts_struct(1).name, '.mat']), 'ts_struct')
     
     
 end
+
+end
+
+function time = monthly_time_from_year(year, npoints)
+
+% this function creates a list of "npoints" monthly serial date numbers, 
+% starting from the "year"
+
+date_vec = repmat([year, zeros(1, 5)], npoints, 1);
+add_points = repmat(1:12, 1, ceil(npoints/12))';
+date_vec(:, 2) = add_points(1:npoints);
+
+time = datenum(date_vec);
 
 end
