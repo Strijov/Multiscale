7aa68fa33249588113b6135a2ee817087d7404da
Anastasia874 <anastasia.motrenko@gmail.com>
2016-06-04 11:14:16 +0300

    ~~documentation

--- code/utils/calcErrorsByModel.m
+++ code/utils/calcErrorsByModel.m
@@ -1,5 +1,8 @@
 function [MAPE_test, MAPE_train, AIC, model] = calcErrorsByModel(StructTS, model, idxTrain, idxTest)
 
+% This funtions returns forecasting errors on the test and train sets for
+% each models and the models with updated fields.
+
 nModels = numel(model);
 
 MAPE_test = zeros(nModels,1);
@@ -8,8 +11,8 @@ AIC = zeros(nModels,1);
 
 for i = 1:nModels
     disp(['Fitting model: ', model(i).name])
-    [MAPE_test(i), MAPE_train(i), model(i)] = ComputeForecastingErrors(...
-                                            StructTS, 0, model(i), ...
+    [MAPE_test(i), MAPE_train(i), model(i)] = computeForecastingErrors(...
+                                            StructTS, model(i), 0, ...
                                             idxTrain, idxTest);
     AIC(i) = 2*StructTS.deltaTp + size(StructTS.matrix, 1) * ...
                         log(nan_norm(StructTS.x(StructTS.deltaTp + 1:end) - ...
