c4c9e36556afa33291078045ff3125750a87b7bb
Anastasia874 <anastasia.motrenko@gmail.com>
2016-06-04 11:34:42 +0300

    rename

---
+++ code/computeForecastingErrors.m
@@ -0,0 +1,75 @@
+function [testMAPE, trainMAPE, model] = computeForecastingErrors(ts, ...
+                                                model, ...
+                                                alpha_coeff,...
+                                                idxTrain,...
+                                                idxVal)
+
+
+% The function fits the foreasting model to the target time series on the
+% train set and applies it to foreast the test set
+% Input:
+% ts - basic time series structure (for more details see createRegMatrix.m)
+%       ts.matrix [m x n] stores the design matrix, where the last [m x
+%       ts.deltaTr] columns store the target variable
+% model - basic model structure (see Systemdocs.doc)
+%       model.handle: handle to the forecasting model, 
+%       model.params: model parameters
+%       model.testErrors, model.trainErrors: forecasting errors (symMAPE)
+%       model.forecasted_y: [(T- (n - ts.deltaTr) x 1)] forcasted values of the
+%       original time series ts.x [T x 1]
+% alpha_coeff - test to train ratio in train-test-validation split. Takes values in 
+%       range [0, 1]. "0" coressponds to no test set, only validetion FIXIT
+% idxTrain - row indices of ts.matrix
+% idxVal   - row indices of ts.matrix. FIXIT Validation set is used as test
+%        set...
+
+if nargin < 4
+    [idxTrain, ~, idxVal, idxX, idxY] = FullSplit(size(ts.matrix, 1), ...
+                            size(ts.matrix, 2), alpha_coeff, ts.deltaTr);
+else
+    idxX = 1:size(ts.matrix, 2) - ts.deltaTr;
+    idxY = size(ts.matrix, 2) - ts.deltaTr + 1: size(ts.matrix, 2);
+end
+
+[forecastY, trainForecastY, model] = feval(model.handle, ts.matrix(idxVal, idxX), model, ...
+                            ts.matrix(idxTrain, idxX), ts.matrix(idxTrain, idxY)); 
+
+% Remember that forecasts and ts.matrix are normalized
+trainMAPE = calcSymMAPE(ts.matrix(idxTrain, idxY), trainForecastY);
+testMAPE = calcSymMAPE(ts.matrix(idxVal, idxY), forecastY);
+forecasts = zeros(size(ts.matrix, 1), ts.deltaTr);
+forecasts(idxTrain, :) = trainForecastY;
+forecasts(idxVal, :) = forecastY;
+forecasts = unravel_target_var(forecasts);
+% Denormalize forecasts:
+model.forecasted_y = forecasts*ts.norm_div + ts.norm_subt;
+model.testError = testMAPE;
+model.trainError = trainMAPE;
+
+
+
+
+end
+
+% DISCUSS: Compute quality of forecast in extra function? Additional quality/error
+% functions?
+
+
+function vecY = unravel_forecasts(trainY, testY, idxTrain, idxTest, deltaTr)
+
+vecY = zeros((numel(idxTrain) + numel(idxTest))*deltaTr, 1);
+idxTrain = repmat(idxTrain(:), 1, deltaTr)' + repmat(0:deltaTr-1, numel(idxTrain), 1)';
+idxTrain = idxTrain(:);
+idxTest = repmat(idxTest(:), 1, deltaTr)' + repmat(0:deltaTr-1, numel(idxTest), 1)';
+idxTest = idxTest(:);
+
+vecY(idxTrain) = reshape((trainY));
+vecY(idxTest) = testY;
+    
+end
+
+
+         
+        
+        
+    
