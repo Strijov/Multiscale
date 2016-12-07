5fab6305611d391dac38cc4cc03f204d1714f815
Anastasia874 <anastasia.motrenko@gmail.com>
2016-12-07 12:57:32 +0300

    changed data paths

--- python-code/utils_.py
+++ python-code/utils_.py
@@ -12,7 +12,7 @@ def parse_options():
 
     parser.add_option('-f', '--filename',
                       type='string',
-                      default=os.path.join('..', 'code','data', 'IotTemplate', 'data.csv'),
+                      default=os.path.join('..','data', 'IotTemplate', 'data.csv'),
                       help='.csv file with input data. Default: %default')
     parser.add_option('-l', '--line-indices',
                       type='string', default="all",#"15, 16",
@@ -57,7 +57,7 @@ def safe_read_iot_data(file_name, line_indices, header, default="EnergyWeather",
             ts_struct = random_data.create_iot_data(n_ts=3, n_req=2, n_hist=7, max_length=5000,
                                                             min_length=2000, slope=0.0001, trend_noise=0.001)
         else:
-            load_raw = not os.path.exists(os.path.join("ProcessedData", "EnergyWeather_orig_train.pkl"))
+            load_raw = not os.path.exists(os.path.join("../data/ProcessedData", "EnergyWeather_orig_train.pkl"))
             ts_struct = load_time_series.load_all_time_series(datasets=['EnergyWeather'], load_raw=load_raw,
                                                               name_pattern="missing")[0]
 
