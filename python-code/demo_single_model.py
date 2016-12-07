633535656234ee9739058b2860dd2afb800a4128
Anastasia874 <anastasia.motrenko@gmail.com>
2016-12-07 16:03:06 +0300

    updated docstring

--- python-code/demo_single_model.py
+++ python-code/demo_single_model.py
@@ -10,29 +10,20 @@ from Forecasting import frc_class, LSTM
 import my_plots
 
 
-
 def main(frc_model=None, generator=None, selector=None):
 
     # Experiment settings.
     TRAIN_TEST_RATIO = 0.75
-    N_PREDICTIONS = 10 # plotting par
+    N_PREDICTIONS = 10  # plotting par
 
     # Load and prepare dataset.
-    load_raw = not os.path.exists(os.path.join("ProcessedData", "EnergyWeather_orig_train.pkl"))
+    load_raw = True  # not os.path.exists(os.path.join("ProcessedData", "EnergyWeather_orig_train.pkl"))
     ts_struct_list = load_time_series.load_all_time_series(datasets='EnergyWeather', load_raw=load_raw, name_pattern="")
 
-
-
-
-    if generator is None:
-        #generator = frc_class.CustomModel(name='Poly', fitfunc=None, predictfunc=None, replace=True, ndegrees=3)
-        generator = frc_class.CustomModel(frc_class.IdentityGenerator, name="IdentityGenerator")
-    if selector is None:
-        #selector = frc_class.CustomModel(name="Identity", fitfunc=None, predictfunc=drop_cols)
-        selector = None
     if frc_model is None:
         frc_model = frc_class.CustomModel(Lasso, name="Lasso", alpha=0.01) # LSTM.LSTM() #frc_class.IdenitityFrc() #LinearRegression()
-    # Create regression matrix
+    # Create regression model
+    model = frc_class.PipelineModel(gen_mdl=generator, sel_mdl=selector, frc_mdl=frc_model)
 
     results = []
     res_text = []
@@ -41,20 +32,18 @@ def main(frc_model=None, generator=None, selector=None):
         # Create regression matrix
         data.create_matrix(nsteps=1, norm_flag=True)
 
-
         # Split data for training and testing
         data.train_test_split(TRAIN_TEST_RATIO)
-        model, frc, gen, sel = data.train_model(frc_model=frc_model, generator=generator, selector=selector) # model parameters are changed inside
+        model, frc, gen, sel = model.train_model(data.trainX, data.trainY)
+        #model, frc, gen, sel = data.train_model(frc_model=frc_model, generator=generator, selector=selector) # model parameters are changed inside
 
-        # data.forecasts returns model obj, forecasted rows of Y matrix and a list [nts] of "flat"/ts indices of forecasted points
         data.forecast(model, data.idx_test, replace=True)
         data.forecast(model, data.idx_train, replace=True)
 
-
-        train_mae = data.mae(idx_rows=data.idx_train)#, out="Training")
-        train_mape = data.mape(idx_rows=data.idx_train)#, out="Training")
-        test_mae = data.mae(idx_rows=data.idx_test)#, out="Test")
-        test_mape = data.mape(idx_rows=data.idx_test)#, out="Test")
+        train_mae = data.mae(idx_rows=data.idx_train, idx_original=data.original_index)
+        train_mape = data.mape(idx_rows=data.idx_train, idx_original=data.original_index)
+        test_mae = data.mae(idx_rows=data.idx_test, idx_original=data.original_index)
+        test_mape = data.mape(idx_rows=data.idx_test, idx_original=data.original_index)
 
         res1 = pd.DataFrame(train_mae, index=[t.name for t in ts.data], columns=[("MAE", "train")])
         res2 = pd.DataFrame(train_mape, index=[t.name for t in ts.data], columns=[("MAPE", "train")])
@@ -64,18 +53,13 @@ def main(frc_model=None, generator=None, selector=None):
         print(res)
 
         results.append(res)
-        #res_text.append(ts.name + frc.name + ":, feature generation: " + gen.name + ", " + sel.name)
+        res_text.append(ts.name)
 
         data.plot_frc(n_frc=N_PREDICTIONS)
 
     my_plots.save_to_latex(results, df_names=res_text)
 
-
-
     return results
-
-
-
 
 if __name__ == '__main__':
     main()
--- python-code/forecast_iot_data.py
+++ python-code/forecast_iot_data.py
@@ -4,12 +4,13 @@ from __future__ import print_function
 
 import pandas as pd
 import utils_
+import my_plots
 
 from sklearn.linear_model import Lasso
 
 from RegressionMatrix import regression_matrix
 from Forecasting import frc_class
-# from Forecasting import  LSTM, GatingEnsemble
+from Forecasting import LSTM, GatingEnsemble
 
 def main(file_name, line_indices, header):
     """
@@ -27,26 +28,33 @@ def main(file_name, line_indices, header):
     TRAIN_TEST_RATIO = 0.75
     N_PREDICTIONS = 10
     N_EXPERTS = 4
+    VERBOSE = True
 
+    # frc_model = frc_class.CustomModel(Lasso, name="Lasso", alpha=0.001)
+    # frc_model = frc_class.CustomModel(GatingEnsemble.GatingEnsemble,
+    #                                  estimators = [LinearRegression() for i in range(N_EXPERTS)])#(LSTM.LSTM, name="LSTM")
 
-    ts = utils_.safe_read_iot_data(file_name=file_name, line_indices=line_indices, header=header, default="poisson")
+    ts = utils_.safe_read_iot_data(file_name=file_name, line_indices=line_indices, header=header, default="poisson", verbose=VERBOSE)
+    if VERBOSE:
+        print(ts.summarize_ts())
+
+    # my_plots.plot_multiple_ts(ts.data, shared_x=True)
     data = regression_matrix.RegMatrix(ts)
     # Create regression matrix
     data.create_matrix(nsteps=1, norm_flag=True)
-
-    #frc_model = frc_class.CustomModel(Lasso, name="Lasso", alpha=0.01)
-    frc_model = frc_class.CustomModel(Lasso, name="Lasso", alpha=0.001)
-    #frc_model = frc_class.CustomModel(GatingEnsemble.GatingEnsemble,
-    #                                  estimators = [LinearRegression() for i in range(N_EXPERTS)])#(LSTM.LSTM, name="LSTM")
-
     # Split data for training and testing
     data.train_test_split(TRAIN_TEST_RATIO)
-    model, _, _, _ = data.train_model(frc_model=frc_model, generator=None,
-                             selector=None)  # model parameters are changed inside
 
-    print("Features before generation:", data.feature_dict)
-    print("Features after generation:", model.named_steps["gen"].feature_dict)
-    print("Features after generation:", model.named_steps["sel"].feature_dict)
+    lr_list = [2e-6, 2e-5, 2e-4]
+    n_lstm_units = [20, 30, 40, 50]
+    hyperpars = {"learning_rate": lr_list, "n_lstm_units": n_lstm_units}
+    frc_model = frc_class.CustomModel(LSTM.LSTM, name="LSTM", n_epochs=20, plot_loss=True)
+    model = frc_class.PipelineModel(frc_mdl=frc_model)
+
+    model, frc, _, _ = model.train_model(data.trainX, data.trainY, hyperpars=hyperpars, n_cvs=5)  # model parameters are changed inside
+
+    if hasattr(frc, "fig"):
+        frc.fig.savefig("fitting_learn_rate_{}.png".format(frc.learning_rate))
 
 
     # data.forecasts returns model obj, forecasted rows of Y matrix and a list [nts] of "flat"/ts indices of forecasted points
@@ -62,15 +70,31 @@ def main(file_name, line_indices, header):
     res3 = pd.DataFrame(test_mae, index=[t.name for t in ts.data], columns=[("MAE", "test")])
     res4 = pd.DataFrame(test_mape, index=[t.name for t in ts.data], columns=[("MAPE", "test")])
     res = pd.concat([res1, res2, res3, res4], axis=1)
+    print("LSTM")
     print(res)
 
     data.plot_frc(n_frc=N_PREDICTIONS)
 
+    frc_model = frc_class.CustomModel(Lasso, name="Lasso", alpha=0.001)
+    model = frc_class.PipelineModel(frc_mdl=frc_model)
+    model,_,_,_ = model.train_model(data.trainX, data.trainY)
+    data.forecast(model, replace=True)
+
+    train_mae = data.mae(idx_rows=data.idx_train)
+    train_mape = data.mape(idx_rows=data.idx_train)
+    test_mae = data.mae(idx_rows=data.idx_test)
+    test_mape = data.mape(idx_rows=data.idx_test)
+
+
+    res1 = pd.DataFrame(train_mae, index=[t.name for t in ts.data], columns=[("MAE", "train")])
+    res2 = pd.DataFrame(train_mape, index=[t.name for t in ts.data], columns=[("MAPE", "train")])
+    res3 = pd.DataFrame(test_mae, index=[t.name for t in ts.data], columns=[("MAE", "test")])
+    res4 = pd.DataFrame(test_mape, index=[t.name for t in ts.data], columns=[("MAPE", "test")])
+    res = pd.concat([res1, res2, res3, res4], axis=1)
+    print("Lasso")
+    print(res)
+
     return res
-
-
-
-
 
 
 if __name__ == '__main__':
